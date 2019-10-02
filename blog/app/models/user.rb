dd1d97e40424dd3e49e6649bd40deac0a9dd5bcc
ToniCP <tonicp97@gmail.com>
2019-10-02 15:22:40 -0500

    created recently user

---
+++ blog/app/models/user.rb
@@ -0,0 +1,15 @@
+class User
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  
+  field :email, type: String
+  field :name, type: String
+  field :last_name, type: String
+  field :mobile_phone, type: String
+
+  validates :name, presence: true
+
+
+scope :recently, -> { where(created_at: 1.month.ago.beginning_of_day..Time.now.end_of_day) }
+
+end
---
+++ blog/config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+	delete '/users/:id',  to: 'users#destroy', as: 'destroy'
+	 
+  resources :users, except: :destroy do 
+  	collection do
+      get 'recently'
+    end
+  end
+   
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
